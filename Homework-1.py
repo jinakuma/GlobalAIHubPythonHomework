75a22f3077779eefd5dd16c5e537ff0cd016f216
Murat UTLU <45118440+jinakuma@users.noreply.github.com>
2020-12-22 08:45:28 +0300

    Create Homework-1.py

---
+++ Homework-1.py
@@ -0,0 +1,7 @@
+print("Lütfen 5 adet sayı giriniz:")
+sayilar=[]
+for i in range(5):
+    value = input()
+    sayilar.append(x)
+for i in range(1,6):
+	print(f"{i}. sayı: {sayılar[i-1]}, {i}. sayı veri tipi{type(sayılar[i-1])}" )
